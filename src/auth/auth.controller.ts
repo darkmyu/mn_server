04301eee768b16311d86645f0af863cc9ec5264c
bammyu <mbjun3630@naver.com>
2025-06-23 14:45:05 +0900

    refactor: save token cookie function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,27 +17,6 @@ export class AuthController {
   ) {}
 
   @Public()
-  @Post('register')
-  @UseGuards(AuthGuard('jwt-register'))
-  async register(@GetUser() oauthUser: OAuthUser, @Body() body: RegisterRequestDto, @Res() res: Response) {
-    const user = await this.authService.register(oauthUser, body);
-    const { accessToken, refreshToken } = await this.authService.generateTokens({
-      id: user.id,
-      username: user.username,
-    });
-
-    this.setTokenCookie(res, 'access_token', accessToken, {
-      maxAge: 60 * 60 * 1000,
-    });
-
-    this.setTokenCookie(res, 'refresh_token', refreshToken, {
-      maxAge: 60 * 60 * 1000 * 24 * 7,
-    });
-
-    return res.send(user);
-  }
-
-  @Public()
   @Get('google')
   @UseGuards(AuthGuard('google'))
   async google() {}
@@ -73,40 +52,53 @@ export class AuthController {
     return this.handleSocialCallback(user, res);
   }
 
-  private async handleSocialCallback(user: User | OAuthUser, res: Response) {
-    const host = this.appConfigService.host;
+  @Public()
+  @Post('register')
+  @UseGuards(AuthGuard('jwt-register'))
+  async register(@GetUser() oauthUser: OAuthUser, @Body() body: RegisterRequestDto, @Res() res: Response) {
+    const user = await this.authService.register(oauthUser, body);
+    await this.setTokenCookies(user, res);
 
-    if ('id' in user) {
-      const { accessToken, refreshToken } = await this.authService.generateTokens({
-        id: user.id,
-        username: user.username,
-      });
-
-      this.setTokenCookie(res, 'access_token', accessToken, {
-        maxAge: 60 * 60 * 1000,
-      });
-
-      this.setTokenCookie(res, 'refresh_token', refreshToken, {
-        maxAge: 60 * 60 * 1000 * 24 * 7,
-      });
-
-      return res.redirect(host);
-    }
-
-    const registerToken = await this.authService.generateRegisterToken(user);
-
-    this.setTokenCookie(res, 'register_token', registerToken, {
-      maxAge: 60 * 60 * 1000,
-    });
-
-    return res.redirect(`${host}/onboarding`);
+    return res.send(user);
   }
 
-  private setTokenCookie(res: Response, name: string, token: string, options: CookieOptions) {
+  private setCookie(res: Response, name: string, token: string, options: CookieOptions) {
     res.cookie(name, token, {
       httpOnly: true,
       domain: this.appConfigService.domain,
       ...options,
     });
+  }
+
+  private async setTokenCookies(user: User, res: Response) {
+    const { accessToken, refreshToken } = await this.authService.generateTokens({
+      id: user.id,
+      username: user.username,
+    });
+
+    this.setCookie(res, 'access_token', accessToken, {
+      maxAge: 60 * 60 * 1000,
+    });
+
+    this.setCookie(res, 'refresh_token', refreshToken, {
+      maxAge: 60 * 60 * 1000 * 24 * 7,
+    });
+  }
+
+  private async handleSocialCallback(user: User | OAuthUser, res: Response) {
+    const host = this.appConfigService.host;
+
+    if ('id' in user) {
+      await this.setTokenCookies(user, res);
+      return res.redirect(host);
+    }
+
+    const registerToken = await this.authService.generateRegisterToken(user);
+
+    this.setCookie(res, 'register_token', registerToken, {
+      maxAge: 60 * 60 * 1000,
+    });
+
+    return res.redirect(`${host}/onboarding`);
   }
 }
