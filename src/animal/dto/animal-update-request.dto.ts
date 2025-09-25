3eaf3439f0042651b7f89166c2d6712b65f69ad9
bammyu <mbjun3630@naver.com>
2025-09-25 17:49:10 +0900

    fix: change import to generate swagger docs

--- src/animal/dto/animal-update-request.dto.ts
+++ src/animal/dto/animal-update-request.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { AnimalCreateRequest } from './animal-create-request.dto';
 
 export class AnimalUpdateRequest extends PartialType(AnimalCreateRequest) {}
