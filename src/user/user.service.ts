ccee1bd19681de6593aefaad862b7dd24df85775
bammyu <mbjun3630@naver.com>
2025-10-29 12:19:26 +0900

    fix: add orderby to user animals query

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -33,6 +33,9 @@ export class UserService {
             user: true,
             breed: true,
           },
+          orderBy: {
+            createdAt: 'asc',
+          },
         },
       },
     });
